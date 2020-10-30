804a3d71f35c51b0b7b93bc9bcaf62f131296d05
Ben Shemmeld <bshemmeld@gmail.com>
2020-10-30 21:41:25 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArrangingCatsAndDogsMathsPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var possibleArrangments = Math.Pow(2, 10);
+            var validArrangements = 0;
+
+            for(var i=0;i < possibleArrangments; i++)
+            {
+                var binaryString = Convert.ToString(i, 2);
+                binaryString = $"{int.Parse(binaryString):0000000000}";
+                
+                if(!binaryString.Contains("00"))
+                {
+                    binaryString = binaryString.Replace("0", " Cat ").Replace("1", " Dog ");
+                    Console.WriteLine(binaryString.Trim());
+                    validArrangements++;
+                }
+            }
+
+            Console.WriteLine($"Found {validArrangements} possible arrangements");
+        }
+    }
+}
